8ba0400b7ba661acc44d18c2fe089fe323279a57
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2019-03-02 22:57:58 +0100

    move extension functions to file where they are used

--- src/main/kotlin/lukasz/aoc18/day1/Day0102.kt
+++ src/main/kotlin/lukasz/aoc18/day1/Day0102.kt
@@ -1,7 +1,5 @@
 package lukasz.aoc18.day1
 
-import extensions.findDuplicate
-import extensions.scan
 import java.io.File
 
 fun main() {
@@ -31,4 +29,32 @@ fun solve02(input: List<Int>): Int? {
     } while (duplicate == null)
 
     return duplicate
+}
+
+/**
+ * Iterates over a [List] while performing an accumulating [operation] on consecutive elements.
+ *
+ * Returns a List containing all intermediate results of the operation.
+ */
+private fun <T> List<T>.scan(initial: T, operation: (T, T) -> T): List<T> {
+    var accumulator = operation(initial, first())
+
+    return mapIndexed { index, element ->
+        if (index != 0) accumulator = operation(accumulator, element)
+        accumulator
+    }
+}
+
+/**
+ * Finds first duplicate element in a [List].
+ *
+ * Searches for duplicates of elements located between [start] index and the end of the list.
+ * Returns first such element which is a duplicate of one of the elements located between
+ * beginning of the list and `start - 1`. Returns `null` if no such element has been found.
+ */
+private fun <T> List<T>.findDuplicate(start: Int): T? {
+    forEachIndexed { index, element ->
+        if (index >= start && subList(0, index).contains(element)) return element
+    }
+    return null
 }
